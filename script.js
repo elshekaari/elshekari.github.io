81f332a528545e90eaf0006aedbaeab98bc735f3
elshekaari <elavashak23@gmail.com>
2025-11-02 22:44:04 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
    HOME BACKGROUND SLIDESHOW
 ======================================== */
 const slides = [
-  { type: 'video', src: 'media/bg1.mp4' },
+  { type: 'video', src: 'elshekari.github.io/media
+/bg1.mp4' },
   { type: 'image', src: 'media/bg2.jpg' },
   { type: 'video', src: 'media/bg3.mp4' }
 ];
